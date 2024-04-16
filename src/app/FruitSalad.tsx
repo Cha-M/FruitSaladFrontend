b033e605b0b5691254d06e88d339b09d01c2b5f5
charlie <charlie@charlie.com>
2024-04-16 22:23:57 +0100

    save

---
+++ src/app/FruitSalad.tsx
@@ -0,0 +1,258 @@
+"use client";
+
+import React, { useEffect, useState, FC } from "react";
+import fruitSaladAbi from "./abi/FruitSalad.json";
+import { MetaMaskInpageProvider } from "@metamask/providers";
+import { ethers, Contract, formatEther } from "ethers";
+
+declare global {
+  interface Window {
+    ethereum?: MetaMaskInpageProvider;
+  }
+}
+
+const fruitSaladAddress = "0x1fc33Fa1249557C007a6F25f028dC71b1920f3F7";
+
+interface AccountProps {
+  account: string | null;
+  balance: string | null;
+  refreshBalances(): any;
+}
+
+const Account = ({ account, balance, refreshBalances }: AccountProps) =>
+  account ? (
+    <div className="z-10 w-full max-w-5xl flex-row text-sm lg:flex space-x-2">
+      <div className="w-full rounded-lg bg-gray-100 p-4 shadow-md whitespace-nowrap flex-initial">
+        Account: {account}
+      </div>
+      <div className="w-full rounded-lg bg-gray-100 p-4 shadow-md whitespace-nowrap flex-initial">
+        Balance: {balance}
+      </div>
+      <button
+        onClick={refreshBalances}
+        className="rounded-lg bg-blue-200 hover:bg-blue-100 p-4 font-bold shadow-md whitespace-nowrap flex-1"
+      >
+        Refresh
+      </button>
+    </div>
+  ) : (
+    <div className="z-10 w-full max-w-5xl flex-row text-sm lg:flex space-x-2">
+      No account
+    </div>
+  );
+
+interface FruitExchangeProps {
+  saladFruits: {
+    Mango: string;
+    Orange: string;
+    Banana: string;
+    Peach: string;
+    Grape: string;
+    FruitSalad: string;
+  };
+  buyOneSaladFruit(): any;
+  makeFruitSalad(): any;
+}
+
+const FruitExchange = ({
+  saladFruits,
+  buyOneSaladFruit,
+  makeFruitSalad,
+}: FruitExchangeProps) => (
+  <div className="z-10 w-full max-w-5xl items-center justify-between text-sm lg:flex mt-2">
+    <div>
+      <button
+        onClick={buyOneSaladFruit}
+        className="rounded-lg bg-blue-200 hover:bg-blue-100 p-4 font-bold shadow-md"
+      >
+        Buy fruit
+      </button>
+    </div>
+    <div className="space-y-2">
+      <div className="rounded-lg bg-gray-100 p-4 w-30 shadow-md">
+        🥭 Mangoes: {saladFruits.Mango}
+      </div>
+      <div className="rounded-lg bg-gray-100 p-4 w-30 shadow-md">
+        🍊 Oranges: {saladFruits.Orange}
+      </div>
+      <div className="rounded-lg bg-gray-100 p-4 w-30 shadow-md">
+        🍌 Bananas: {saladFruits.Banana}
+      </div>
+      <div className="rounded-lg bg-gray-100 p-4 w-30 shadow-md">
+        🍑 Peaches: {saladFruits.Peach}
+      </div>
+      <div className="rounded-lg bg-gray-100 p-4 w-30 shadow-md">
+        🍇 Grapes: {saladFruits.Grape}
+      </div>
+    </div>
+    <button
+      onClick={makeFruitSalad}
+      className="rounded-lg bg-blue-200 hover:bg-blue-100 p-4 font-bold shadow-md"
+    >
+      Mix fruit
+    </button>
+    <div className="rounded-lg bg-gray-100 p-4 w-30 shadow-md">
+      🥣 FruitSalad: {saladFruits.FruitSalad}
+    </div>
+  </div>
+);
+
+const FruitSalad = () => {
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const [defaultAccount, setDefaultAccount] = useState<string | null>(null);
+  const [fruitSaladContract, setFruitSaladContract] = useState<Contract | null>(
+    null
+  );
+  const [fruitSaladContractWithSigner, setFruitSaladContractWithSigner] =
+    useState<Contract | null>(null);
+  const [etherBalance, setEtherBalance] = useState<string>("?");
+
+  const [saladFruits, setSaladFruits] = useState<{
+    Mango: string;
+    Orange: string;
+    Banana: string;
+    Peach: string;
+    Grape: string;
+    FruitSalad: string;
+  }>({
+    Mango: "?",
+    Orange: "?",
+    Banana: "?",
+    Peach: "?",
+    Grape: "?",
+    FruitSalad: "?",
+  });
+
+  const connectWallet = () => {
+    if (window.ethereum) {
+      window.ethereum
+        .request({
+          method: "eth_requestAccounts",
+        })
+        .then((result: any) => {
+          changeAccount(result[0]);
+          setErrorMessage("Wallet connected");
+        });
+    } else {
+      setErrorMessage("Need to install MetaMask");
+    }
+  };
+
+  const changeAccount = (newAccount: string) => {
+    setDefaultAccount(newAccount);
+    updateContracts();
+  };
+
+  const updateContracts = async () => {
+    if (window.ethereum) {
+      let _provider: ethers.BrowserProvider = new ethers.BrowserProvider(
+        window.ethereum
+      );
+      let _signer: ethers.JsonRpcSigner = await _provider.getSigner();
+      let sepoliaProvider = ethers.getDefaultProvider("sepolia");
+
+      setFruitSaladContract(
+        new Contract(fruitSaladAddress, fruitSaladAbi, sepoliaProvider)
+      );
+      setFruitSaladContractWithSigner(
+        new Contract(fruitSaladAddress, fruitSaladAbi, _signer)
+      );
+    }
+  };
+
+  const refreshBalances = async () => {
+    if (
+      window.ethereum &&
+      defaultAccount &&
+      fruitSaladContractWithSigner &&
+      fruitSaladContract
+    ) {
+      try {
+        let _provider: ethers.BrowserProvider = new ethers.BrowserProvider(
+          window.ethereum
+        );
+        setEtherBalance(
+          `${formatEther(await _provider.getBalance(defaultAccount))} ETH`
+        );
+
+        setSaladFruits({
+          Mango: `${await fruitSaladContractWithSigner.balanceOf(
+            defaultAccount,
+            0
+          )}`,
+          Orange: `${await fruitSaladContractWithSigner.balanceOf(
+            defaultAccount,
+            1
+          )}`,
+          Banana: `${await fruitSaladContractWithSigner.balanceOf(
+            defaultAccount,
+            2
+          )}`,
+          Peach: `${await fruitSaladContractWithSigner.balanceOf(
+            defaultAccount,
+            3
+          )}`,
+          Grape: `${await fruitSaladContractWithSigner.balanceOf(
+            defaultAccount,
+            4
+          )}`,
+          FruitSalad: `${await fruitSaladContractWithSigner.balanceOf(
+            defaultAccount,
+            5
+          )}`,
+        });
+      } catch (error) {
+        console.error("refreshBalances catch", error);
+      }
+    }
+  };
+
+  const buyOneSaladFruit = async () => {
+    if (fruitSaladContractWithSigner) {
+      try {
+        const oneFruitUpdate: any = await fruitSaladContractWithSigner.buyFruit(
+          1,
+          {
+            value: 500000000000000,
+          }
+        );
+        await oneFruitUpdate.wait();
+      } catch (error) {
+        console.error("buyTenSaladFruit catch", error);
+      }
+    }
+  };
+
+  const makeFruitSalad = async () => {
+    if (fruitSaladContractWithSigner) {
+      try {
+        const fruitSaladUpdate: any =
+          await fruitSaladContractWithSigner.makeFruitSalad(1);
+        await fruitSaladUpdate.wait();
+      } catch (error) {
+        console.error("fruitSaladUpdate catch", error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    connectWallet();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div className="z-10 w-full max-w-5xl flex-col text-sm lg:flex space-y-4">
+      <Account
+        account={defaultAccount}
+        balance={etherBalance}
+        refreshBalances={refreshBalances}
+      />
+      <FruitExchange
+        saladFruits={saladFruits}
+        buyOneSaladFruit={buyOneSaladFruit}
+        makeFruitSalad={makeFruitSalad}
+      />
+    </div>
+  );
+};
+export default FruitSalad;
---
+++ src/app/blurb.tsx
@@ -0,0 +1,13 @@
+const Blurb = () => (
+  <div className="rounded-lg bg-gray-100 p-4 shadow-md">
+    <b>FruitSalad</b> is an ERC1155 token on the Ethereum blockchain (Sepolia
+    testnet). After buying a set of the fruit tokens for xxx Ether, you can then
+    make <b>FruitSalad</b>. To make one fruit salad token you need{" "}
+    <b>one mango</b>, <b>two oranges</b>, <b>one banana</b>, <b>one peach</b>{" "}
+    and <b>ten grapes</b>. The number of fruit tokens you receive on buying a
+    set will be pseudo-random. Hit <b>Refresh</b> after each transaction is
+    confirmed to see your updated balances.
+  </div>
+);
+
+export default Blurb;
