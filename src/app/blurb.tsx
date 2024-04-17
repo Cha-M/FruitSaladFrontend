2bca57602ce17f73534bd1d8486d89ba052447be
charlie <charlie@charlie.com>
2024-04-17 17:03:21 +0100

    typo

--- src/app/blurb.tsx
+++ src/app/blurb.tsx
@@ -3,7 +3,7 @@ const Blurb = () => (
     <b>FruitSalad</b> is an ERC1155 token on the Ethereum blockchain (Sepolia
     testnet). After buying a set of the fruit tokens for 0.0005 Ether, you can
     then make <b>FruitSalad</b>. To make one fruit salad token you need{" "}
-    <b>one mango</b>, <b>two oranges</b>, <b>one banana</b>, <b>one peach</b>{" "}
+    <b>one mango</b>, <b>two oranges</b>, <b>one banana</b>, <b>two peaches</b>{" "}
     and <b>ten grapes</b>. The number of fruit tokens you receive on buying a
     set will be pseudo-random. Hit <b>Refresh</b> after each transaction is
     confirmed to see your updated balances.
